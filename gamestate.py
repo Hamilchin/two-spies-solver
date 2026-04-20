77adc2cb0c15d9454caa5d28dcdd5c54fad4ab42
Hamilchin <chinalexan@gmail.com>
2026-04-20 02:06:00 -0700

    changes from a yr ago

--- gamestate.py
+++ gamestate.py
@@ -1,20 +1,30 @@
 
-class gameMap:
-    def __init__(self, map_name, player1_start, player2_start):
+class gameState:
+    ACTIONS = ["move", "control"]
+    #move, control, 
+
+    def __init__(self, map_name, locs, intel = (0, 0)):
         if map_name == "four-cities":
             self.map = {1: (2, 3), 2: (1, 3), 3: (1, 2, 4), 4: (3)}
+            self.major_cities = {1, 3}
         
         if player1_start not in self.map.keys:
             raise ValueError("Invalid starting city for player 1")
-
         if player2_start not in self.map.keys:
             raise ValueError("Invalid starting city for player 2")
         
-        self.p1loc = player1_start
-        self.p2loc = player2_start
+        self.locs = [locs[0], locs[1]] #private
+        self.ipt = [0, 0]
+        self.intel [intel[0], intel[1]]
+        self.controlled = [set(), set()]
+        self.turn = 1
+        self.public_actions = [[], []]
 
-        self.p1control = set()
-        self.p2control = set()
+    def get_info_sets(self):
+        current_player = self.turn % 2 + 1
+    
+    def is_terminal(self): #if is-terminal on the start of a turn, current player loses
+        #
 
     def move(self, player, destination):
         if player == 1:
@@ -31,20 +41,17 @@ class gameMap:
     def control(self, player):
         if player == 1:
             self.p1control.append(self.p1loc)
+            if self.p1loc in self.major_cities:
+                self.p1ipt += 4
+            else:
+                self.p1ipt += 1
         elif player == 2:
             self.p2control.append(self.p2loc)
+            if self.p2loc in self.major_cities:
+                self.p2ipt += 4
+            else:
+                self.p2ipt += 1
         else:
             raise ValueError("Invalid player number")
         
-    def 
-
-
         
-
-
-
-
-
-class gameState:
-    def __init__():
-        self.map = map
